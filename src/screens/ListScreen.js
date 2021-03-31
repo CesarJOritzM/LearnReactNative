f00995bf145fec1a191ea23ef11db60c42d8a369
CesarJOritzM <cesarjavierortizmontero@gmail.com>
2021-03-30 22:11:46 -0500

    Hiden Horizontal-Scrollbar

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -9,18 +9,31 @@ const ListScreen = () => {
     { name: 'friend#4' },
     { name: 'friend#5' },
     { name: 'friend#6' },
+    { name: 'friend#7' },
+    { name: 'friend#8' },
+    { name: 'friend#9' },
+    { name: 'friend#10' },
+    { name: 'friend#11' },
+    { name: 'friend#12' },
   ];
   return (
     <View>
       <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(friend) => friend.name}
         data={friends}
         renderItem={({ item }) => {
-          return <Text>{item.name} </Text>;
+          return <Text style={style.textStyle}>{item.name} </Text>;
         }}
       />
     </View>
   );
 };
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50,
+  },
+});
 
 export default ListScreen;
